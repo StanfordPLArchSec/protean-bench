b91b10e90848b98e93cffb2fbfd56f3ef5fd1282
Nicholas Mosier <nmosier@stanford.edu>
2025-01-16 12:59:23 -0800

    initial commit

---
+++ crossbin-simpoint.py
@@ -0,0 +1,111 @@
+#!/usr/bin/python3
+
+import argparse
+import types
+import ninja_syntax
+import os
+
+parser = argparse.ArgumentParser()
+parser.add_argument("cmd")
+parser.add_argument("args", nargs = "*")
+parser.add_argument("--bin", "-b", action = "append", default = [])
+parser.add_argument("--mem-size", default = "512MiB")
+parser.add_argument("--stack-size", default = "8MiB")
+parser.add_argument("--stdin", default = "/dev/null")
+parser.add_argument("--outdir", "-o", required = True)
+parser.add_argument("--gem5-exe", required = True, type = os.path.abspath)
+parser.add_argument("--gem5-configs", required = True, type = os.path.abspath)
+args = parser.parse_args()
+args.args = " ".join(args.args)
+
+def parse_subopts(s):
+    d = dict()
+    for subopt in s.split(","):
+        kvs = subopt.split("=", maxsplit=1)
+        if len(kvs) == 1:
+            kvs.append(None)
+        key, value = kvs
+        d[key] = value
+    return d
+
+# Parse binary specifications
+bins = []
+for bin in args.bin:
+    bin = types.SimpleNamespace(**parse_subopts(bin))
+    bin.cwd = os.path.abspath(bin.cwd)
+    bins.append(bin)
+
+# Gather (shared) gem5 exe args.
+shared_gem5_exe_args = "-re --silent-redirect"
+
+# Gather (shared) gem5 script args.
+shared_gem5_script_args = f"--stdin={args.stdin} --stdout=stdout.txt --stderr=stderr.txt --mem-size={args.mem_size} --max-stack-size={args.stack_size}"
+
+# Create the main outdir and start a build.ninja file in it.
+# Then change directories into there.
+os.makedirs(args.outdir, exist_ok=True)
+f = open(os.path.join(args.outdir, "build.ninja"), "wt")
+ninja = ninja_syntax.Writer(f)
+os.chdir(args.outdir)
+
+# Create subdirectories for each binary, and symlink 'run' to their rundirs.
+for bin in bins:
+    dir = bin.name
+    os.makedirs(dir, exist_ok=True)
+    rundir = os.path.join(dir, "run")
+    # TODO: Make paths relative, if possible?
+    if os.path.exists(rundir):
+        assert os.path.islink(rundir)
+        os.unlink(rundir)
+    os.symlink(os.path.abspath(bin.cwd), rundir)
+
+# Ninja Rules
+def make_gem5_pincpu_run_cmd(config, script_args):
+    return f"if [ -d $outdir ]; then rm -r $outdir; fi && mkdir -p $outdir && /usr/bin/time -vo $outdir/time.txt {args.gem5_exe} {shared_gem5_exe_args} -d $outdir {config} {shared_gem5_script_args} --chdir=$cwd {script_args} -- $bench_exe {args.args}"
+
+ninja.rule(
+    name = "bbhist",
+    command = make_gem5_pincpu_run_cmd(config = os.path.join(args.gem5_configs, "pin-bbhist.py"), script_args = f"--bbhist=$bbhist"),
+    description = "$outdir",
+    restat = True,
+)
+
+# Ninja Builds.
+
+def fixup_cmd_path(bin) -> str:
+    if os.path.isabs(args.cmd):
+        return args.cmd
+    else:
+        cmd = os.path.join(bin.cwd, args.cmd)
+        assert os.path.isfile(cmd)
+        return cmd
+
+def build_binary_bbhist(bin, dir: str, variables: dict):
+    outdir = f"{dir}/bbhist"
+    bbhist_txt = f"{dir}/bbhist.txt"
+    bbhist_py = f"{args.gem5_configs}/pin-bbhist.py"
+    ninja.build(
+        outputs = [bbhist_txt],
+        rule = "bbhist",
+        inputs = [args.gem5_exe, bbhist_py, fixup_cmd_path(bin)],
+        variables = {
+            **variables,
+            "outdir": outdir,
+            "bbhist": bbhist_txt,
+        },
+    )
+
+def build_binary(bin, dir: str):
+    variables = {
+        "cwd": os.path.join(dir, "run"),
+        "bench_exe": fixup_cmd_path(bin),
+    }
+
+    # Phase 1: bbhist
+    build_binary_bbhist(bin = bin, dir = dir, variables = variables)
+
+def build_all():
+    for bin in bins:
+        build_binary(bin = bin, dir = bin.name)
+
+build_all()
