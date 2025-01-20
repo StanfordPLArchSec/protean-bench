76010741886039508684a8e4498f45488542d0cf
Nicholas Mosier <nmosier@stanford.edu>
2025-01-20 08:42:35 -0800

    force overwrite previous ptex.sif

--- apptainer.sh
+++ apptainer.sh
@@ -4,4 +4,4 @@ script_dir="$(dirname "${BASH_SOURCE[0]}")"
 cd "${script_dir}"
 
 docker save -o ptex.tar ptex:latest
-apptainer build ptex.sif docker-archive://ptex.tar
+apptainer build -F ptex.sif docker-archive://ptex.tar
