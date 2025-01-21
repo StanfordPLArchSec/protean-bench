f2b162517b8b7c9ba22090e50d64aa9145a2f411
Nicholas Mosier <nmosier@stanford.edu>
2025-01-21 07:33:48 -0800

    add other helepr scripts

---
+++ helpers/geomean.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+import sys
+import math
+
+lines = sys.stdin.readlines()
+# FIXME: Detect this programmatically.
+lines = lines[1:]
+
+n = len(lines[0].split()) - 1
+
+output = ['geomean']
+for i in range(n):
+    l = list()
+    for line in lines:
+        l.append(float(line.split()[i+1]))
+    output.append(f'{pow(math.prod(l), 1 / len(l)) : .4}')
+for line in lines:
+    print(line, end = '')
+print(*output)
---
+++ helpers/normalize.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import sys
+
+def parsenum(s: str):
+    try:
+        return float(s)
+    except ValueError:
+        return s
+
+for line in sys.stdin:
+    tokens = line.split()
+
+    # Skip empty lines.
+    if len(tokens) == 0:
+        continue
+
+    # If the leader is not an integer,
+    # then just fill the rest of the line
+    # with -'s.
+    leader = parsenum(tokens[1])
+    outl = [tokens[0]]
+    for token in tokens[1:]:
+        value = parsenum(token)
+        if isinstance(value, str):
+            outl.append(value)
+        elif isinstance(leader, str):
+            outl.append("-")
+        else:
+            outl.append(value / leader)
+    print(*outl)
