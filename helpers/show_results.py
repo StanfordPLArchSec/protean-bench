a1a611739b739338d696ed2e17e668a0cbb7c69c
Nicholas Mosier <nmosier@stanford.edu>
2025-01-28 07:15:19 -0800

    update show_results.py

--- helpers/show_results.py
+++ helpers/show_results.py
@@ -9,9 +9,15 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--bench", "-b", action="append", required=True)
 parser.add_argument("--exp", "-e", action="append", required=True)
 parser.add_argument("--quiet", "-q", action="store_true")
+parser.add_argument("--bingroup", "-g", required=True)
+parser.add_argument("--metric", "-m", default="cycles")
+parser.add_argument("--exp-suffix")
 args = parser.parse_args()
 input = "0" # TODO: Don't hard-code this.
-bingroup = "main" # TODO: Don't hard-code this.
+
+if args.exp_suffix:
+    for i, exp in enumerate(args.exp):
+        args.exp[i] = exp + args.exp_suffix
 
 if not args.quiet:
     print("bench", *args.exp)
@@ -22,11 +28,11 @@ for bench in args.bench:
         exit(1)
     line = [bench]
     for exp in args.exp:
-        results_json = os.path.join(bench, "exp", input, bingroup, exp, "results.json")
+        results_json = os.path.join(bench, "exp", input, args.bingroup, exp, "results.json")
         if not os.path.isfile(results_json):
             print(f"file does not exist: {results_json}", file=sys.stderr)
         with open(results_json) as f:
             j = json.load(f)
-        cycles = j["stats"]["cycles"]
-        line.append(cycles)
+        metric = j["stats"][args.metric]
+        line.append(metric)
     print(*line)
