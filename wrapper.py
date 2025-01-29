56ceb0f749bf7ebee08e45543360135f98a1b878
Nicholas Mosier <nmosier@stanford.edu>
2025-01-28 19:13:45 -0800

    update wrapper

--- wrapper.py
+++ wrapper.py
@@ -10,12 +10,16 @@ parser.add_argument("--suite", "-s", action="append", default=[])
 parser.add_argument("--bench", "-b", action="append", default=[])
 parser.add_argument("--exp", "-e", action="append", required=True)
 parser.add_argument("--exp-suffix", action="append", default=None)
+parser.add_argument("--print", "-p", action="store_true")
+parser.add_argument("--skip-bench", action="append", default=[])
 args = parser.parse_args()
 
 # First, get the full list of benchmarks.
 benches = set(args.bench)
 for suite in args.suite:
     benches.update(benchsuites[suite])
+for bench in args.skip_bench:
+    benches.remove(bench)
 # print("Running benchmarks:", " ".join(benches))
 
 # Then, get the full list of experiments.
@@ -35,4 +39,9 @@ for bench in benches:
         result = os.path.join(bench, "exp", "0", args.bingroup, exp, "results.json")
         cmd.append(result)
 
-print(" ".join(cmd))
+if args.print:
+    print(*cmd)
+    exit(0)
+
+os.execlp("./snakemake-slurm-apptainer.sh", *cmd)
+
