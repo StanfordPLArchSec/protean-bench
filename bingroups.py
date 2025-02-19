c46a009290d9433fd00f61ef40f05958848e9056
Nicholas Mosier <nmosier@stanford.edu>
2025-02-18 21:38:05 -0800

    update

--- bingroups.py
+++ bingroups.py
@@ -1,9 +1,3 @@
 bingroups = {
-    "main": ["base", "sni.opt"],
-    "crit": ["base", "sni", "sni.c"],
-    "sink": ["base", "sni", "sni.s", "sni.s.c"],
-    "branch": ["base", "sni", "sni.b", "sni.b.f", "sni.b.f.h", "sni.b.f.h.c"],
-    "cbeval": ["base", "sni", "slh"],
-    "rot": ["base", "sni", "sni.b", "sni.b.f", "sni.b.f.r", "sni.b.f.r.s", "sni.b.f.r.h", "sni.b.f.r.s.h"],
-    "rot2": ["base", "sni.b.f.r.s.h", "sni.b.f.s.h"],
+    "main": ["base", "sni"],
 }
