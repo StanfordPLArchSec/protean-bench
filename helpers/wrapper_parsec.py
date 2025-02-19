d1c18bc3d7d9797a6562906807aaa2576684d066
Nicholas Mosier <nmosier@stanford.edu>
2025-02-18 21:25:45 -0800

    add wrapper helper for PARSEC

---
+++ helpers/wrapper_parsec.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import argparse
+import os
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--exp", "-x", action="append", required=True)
+parser.add_argument("snakemake_cmd", nargs="+")
+args = parser.parse_args()
+
+cmd = [*args.snakemake_cmd]
+benches = [
+    "apps/blackscholes",
+    "apps/bodytrack",
+    "apps/facesim",
+    "apps/ferret",
+    "apps/fluidanimate",
+    "apps/freqmine",
+    "apps/raytrace",
+    "apps/swaptions",
+    "apps/vips",
+    "apps/x264",
+    "kernels/canneal",
+    "kernels/dedup",
+    "kernels/streamcluster",
+]
+for bench in benches:
+    for exp in args.exp:
+        cmd.append(f"parsec/pkgs/{bench}/run/exp/{exp}/stamp.txt")
+
+os.execvp(cmd[0], cmd)
+
