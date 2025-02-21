800babdad051bcb7d8e6e74c681ef3b81cf214d8
Nicholas Mosier <nmosier@stanford.edu>
2025-02-21 13:48:58 -0800

    update

--- helpers/bbv-instcount.py
+++ helpers/bbv-instcount.py
@@ -6,15 +6,17 @@ import sys
 parser = argparse.ArgumentParser()
 args = parser.parse_args()
 
-n = 0
+total_insts = 0
 for line in sys.stdin:
     if not line.startswith("T "):
         continue
     tokens = line.split()[1:]
+    line_insts = 0
     for token in tokens:
         x = token.split(":")[2]
-        n += int(x)
-
-print(n)
+        line_insts += int(x)
+    print(line_insts)
+    total_insts += line_insts
+print(total_insts)
 
                  
