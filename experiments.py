aaeef1727c38840f8ea3e6ea8653d48b116d9a65
Nicholas Mosier <nmosier@stanford.edu>
2025-11-18 10:40:07 -0800

    update

--- experiments.py
+++ experiments.py
@@ -35,18 +35,19 @@ benches_spec_fp = [
     "654.roms_s",
 ]
 
+main_confs = ["base/unsafe"]
+for defense in ["stt", "spt", "sptsb"]:
+    main_confs.append(f"base/{defense}.atret")
+for protmech in ["track", "delay"]:
+    for binary in ["base", "cts", "ct", "nct"]:
+        main_confs.append(f"{binary}/prot{protmech}.atret")
+
 # Experiment 1: SPEC CPU2017 {INT,FP} on a {P,E}-core
 for suite, bench in itertools.chain(
         zip(itertools.repeat("int"), benches_spec_int),
         zip(itertools.repeat("fp"), benches_spec_fp)):
-    confs = ["base/unsafe"]
-    for defense in ["stt", "spt", "sptsb"]:
-        confs.append(f"base/{defense}.atret")
-    for binary in ["base", "cts", "ct", "nct"]:
-        for protmech in ["delay", "track"]:
-            confs.append(f"{binary}/prot{protmech}.atret")
-    for conf in confs:
-        for core in ["p", "e"]:
+    for conf in main_confs:
+        for core in "pe":
             target = f"{bench}/exp/0/main/{conf}.{core}core/results.json"
             experiments[f"general.spec.{suite}"].append(target)
 
@@ -65,16 +66,103 @@ for bench in benches_spec_int:
         target = f"{bench}/exp/0/main/{conf}.pcore/results.json"
         experiments[f"predictor"].append(target)
 
+# Experiment: PARSEC.
+benches_parsec = [
+    "apps/blackscholes",
+    "apps/facesim",
+    "apps/ferret",
+    "apps/fluidanimate",
+    "apps/freqmine",
+    "apps/raytrace",
+    "apps/swaptions",
+    "apps/vips",
+    "apps/x264",
+    "kernels/canneal",
+    "kernels/dedup",
+    "kernels/streamcluster",
+]
+for bench in benches_parsec:
+    for conf in main_confs:
+        experiments["general.parsec"].append(
+            f"parsec/pkgs/{bench}/run/exp/{conf}/stamp.txt")
+
+# Experiment: Ctrl speculation model
+for bench in benches_spec_int:
+    confs = ["base/unsafe"]
+    for defense in ["stt", "spt"]:
+        confs.append(f"base/{defense}.ctrl")
+    for binary in ["base", "ct"]:
+        confs.append(f"{binary}/prottrack.ctrl")
+    for conf in confs:
+        experiments[f"ctrl"].append(
+            f"{bench}/exp/0/main/{conf}.pcore/results.json")
+
+# Experiment: No bugfixes for STT, SPT, SPT-SB.
+for bench in benches_spec_int:
+    confs = ["unsafe"]
+    for defense in ["stt", "spt"]:
+        for suffix in ["", "bug"]:
+            confs.append(f"{defense}{suffix}.atret")
+    for conf in confs:
+        experiments["buggy"].append(
+            f"{bench}/exp/0/main/base/{conf}.pcore/results.json")
+
+# Experiment: ProtCC overhead of ProtCC-CTS/-CT/-UNR
+# binaries.
+for bench in benches_spec_int:
+    for binary in ["base", "cts", "ct", "nct"]:
+        experiments["protcc"].append(
+            f"{bench}/exp/0/main/{binary}/unsafe.pcore/results.json")
+
+# Experiment: ProtL1 variants.
+for bench in benches_spec_int:
+    confs = ["base/unsafe"]
+    for binary in ["base", "ct"]:
+        for protmem in [".shadowmem", "", ".noshadow"]:
+            confs.append(f"{binary}/prottrack{protmem}.atret")
+    for conf in confs:
+        experiments["protmem"].append(
+            f"{bench}/exp/0/main/{conf}.pcore/results.json")
+
+# Experiment: AccessDelay and AccessTrack.
+for bench in benches_spec_int:
+    confs = ["base/unsafe"]
+    for binary in ["base", "ct"]:
+        for mode in ["", ".access"]:
+            confs.append(f"{binary}/prottrack{mode}.atret")
+    for conf in confs:
+        experiments["access"].append(
+            f"{bench}/exp/0/main/{conf}.pcore/results.json")
+
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--exp", action="append", choices=experiments.keys())
-    parser.add_argument("--dry-run", "-n", action="store_true")
-    parser.add_argument("snakemake_command", nargs="+")
+    subparser = parser.add_subparsers(dest="cmd", required=True)
+    subparser_list = subparser.add_parser("list")
+    subparser_run = subparser.add_parser("run")
+    subparser_run.add_argument(
+        "--exp", action="append", choices=experiments.keys())
+    subparser_run.add_argument("--dry-run", "-n", action="store_true")
+    subparser_run.add_argument("--list", "-l", action="store_true")
+    subparser_run.add_argument("snakemake_command", nargs="+")
     args = parser.parse_args()
+
+    if args.cmd == "list":
+        for experiment in experiments.keys():
+            print(experiment)
+        return 0
+    assert args.cmd == "run"
+
+    # Collect all matching experiments.
+    requested_experiments = []
+    for pattern in args.exp:
+        for exp in experiments.keys():
+            pat = re.escape(pattern) + r"(\..*)?"
+            if re.fullmatch(pat, exp):
+                requested_experiments.append(exp)
 
     # Collect the snakemake targets.
     targets = []
-    for experiment in args.exp:
+    for experiment in requested_experiments:
         targets.extend(experiments[experiment])
 
     # Run the snakemake command.
