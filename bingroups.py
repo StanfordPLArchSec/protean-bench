0d8b3c2c0ea80a19379f460f64b9ee927754f716
Nicholas Mosier <nmosier@stanford.edu>
2025-11-26 12:37:08 -0800

    update bignrops

--- bingroups.py
+++ bingroups.py
@@ -2,5 +2,5 @@ bingroups = {
     "main": ["base", "cts", "ct", "nct"],
     "ctbench": ["base", "ct"],
     "ctsbench": ["base", "cts"],
-    "nctbench": ["base", "nct", "nctx"],
+    "nctbench": ["base", "nct"],
 }
