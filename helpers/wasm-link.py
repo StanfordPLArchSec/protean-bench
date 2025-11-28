4231382d3e2c55a54fa5d06bd3b15446e1f64a25
Nicholas Mosier <nmosier@stanford.edu>
2025-11-28 13:53:18 -0800

    update

---
+++ helpers/wasm-link.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+
+import argparse
+import subprocess
+import sys
+import os
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--wasm2c", required=True)
+parser.add_argument("--wasm-linker", required=True)
+parser.add_argument("--host-cc", required=True)
+parser.add_argument("--wabt-src", required=True)
+parser.add_argument("--wabt-bin", required=True)
+parser.add_argument("exe")
+parser.add_argument("command", nargs="*")
+args = parser.parse_args()
+
+# Find the output in the command.
+objpath = None
+wasmpath = None
+linkcmd = [args.wasm_linker, *args.command]
+for i, arg in enumerate(linkcmd):
+    if arg == "-o":
+        objpath = linkcmd[i + 1]
+        wasmpath = objpath + ".wasm"
+        linkcmd[i + 1] = wasmpath
+        break
+assert objpath
+
+def runcmd(cmd):
+    cmd = " ".join(cmd)
+    result = subprocess.run(cmd, shell=True)
+    if result.returncode != 0:
+        print(f"failed ({result.returncode}): {cmd}", file=sys.stderr)
+        exit(1)
+
+# Run the wasmlink command.
+runcmd(linkcmd)
+
+# Run the wasm2c command.
+cpath = wasmpath + ".c"
+runcmd([args.wasm2c, wasmpath, "-o", cpath])
+
+# Run the native compile+link command.
+nativecmd = [
+    args.host_cc, cpath, "-o", objpath,
+]
+wasm2c_src = os.path.join(args.wabt_src, "wasm2c")
+for c in ["wasm-rt-impl.c", "uvwasi-rt.c", "wasm-rt-runner-static.c", "wasm-rt-os-unix.c"]:
+    nativecmd.append(os.path.join(wasm2c_src, c))
+nativecmd.append("-I" + os.path.join(args.wabt_src, "third_party", "uvwasi", "include"))
+nativecmd.append("-I" + wasm2c_src)
+nativecmd.append(os.path.join(args.wabt_bin, "third_party", "uvwasi", "libuvwasi_a.a"))
+# TODO: Add a command-line switch to this script enable static compiling.
+nativecmd.append("-luv_a")
+nativecmd.append("-lm")
+runcmd(nativecmd)
