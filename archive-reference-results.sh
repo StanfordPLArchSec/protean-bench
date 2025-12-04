2ba7694de80d9fa10fde0d57431f768a363d8061
Nicholas Mosier <nmosier@stanford.edu>
2025-12-04 15:09:58 -0800

    update

--- archive-reference-results.sh
+++ archive-reference-results.sh
@@ -3,4 +3,8 @@
 set -eu
 
 find ctsbench.* bearssl ctaes djbsort nctbench.openssl.* wasm.4*.* -name results.json -exec cp --parents {} ./reference/ \;
-find webserv/exp/c1r1 -name stats.txt -exec cp --parents {} ./reference/ \;
+find webserv -name stats.txt -exec cp --parents {} ./reference/ \;
+find 6*.*_s -name results.json -exec cp --parents {} ./reference \;
+cp --parents tables/{wasmbench,ctsbench,ctbench,nctbench,webserv}.tex ./reference
+cp --parents results/{protcc-overhead,protl1-variants,spectre-ctrl,baseline-fixes,access}.tex ./reference
+cp --parents figures/*.csv ./reference
